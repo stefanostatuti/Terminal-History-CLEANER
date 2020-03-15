3e24bf8ce0e2f57e54efcfa381c7b3d62becd86c
stefanostatuti <stefano.statuti@hotmail.it>
2020-03-15 16:49:43 +0100

    Update TerminalHistoryCLEANER.sh

--- TerminalHistoryCLEANER.sh
+++ TerminalHistoryCLEANER.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #------------------Configuration parameters---------------#
-homeDir='/home/riso64bit33333'
+homeDir='/home/pi'
 #--------------------End Configuration--------------------#
 clear
 echo "******************************************"
@@ -14,22 +14,22 @@ echo "******************************************"
 echo " "
 if [ ! -d $homeDir ]
 then
-       echo "ERROR: User's folder NOT found, exiting..."
+    echo "ERROR: User's folder NOT found, exiting..."
 else
-	echo "Backupping Original bash History, sorting and writing in new One..."
-	cp $homeDir/.bash_history $homeDir/.bash_history.old
-	cat $homeDir/.bash_history | sort -u >> $homeDir/.bash_history_temp
-	echo "Done."
-	echo " "
-	echo "Changing permissions ... "
-	chmod 600 $homeDir/.bash_history_temp
-	echo "Done."
-	echo ""
-	echo "Copying new and removing Old one..."
-	rm $homeDir/.bash_history
-	mv $homeDir/.bash_history_temp $homeDir/.bash_history
-	echo "Done."
+    echo "Backupping Original bash History, sorting and writing in new One..."
+    cp $homeDir/.bash_history $homeDir/.bash_history.old
+    cat $homeDir/.bash_history | sort -u >> $homeDir/.bash_history_temp
+    echo "Done."
+    echo " "
+    echo "Changing permissions ... "
+    chmod 600 $homeDir/.bash_history_temp
+    echo "Done."
+    echo ""
+    echo "Copying new and removing Old one..."
+    rm $homeDir/.bash_history
+    mv $homeDir/.bash_history_temp $homeDir/.bash_history
+    echo "Done."
 fi
 echo "******************************************"
 echo "*            Bye Bye     :)              *"
-echo "******************************************"
+echo "******************************************"                                                      
